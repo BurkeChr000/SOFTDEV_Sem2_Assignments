9e05df02e997fd722fe4e5ba471e78ca3082639e
BurkeChr000 <burkechr000@warren.k12.in.us>
2024-01-23 10:30:12 -0500

    commit

--- 02_03_Generic Interfaces/Assignment/src/App.java
+++ 02_03_Generic Interfaces/Assignment/src/App.java
@@ -1,5 +1,21 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+
+        Property<String> myStringProperty = new Property<String>();
+        myStringProperty.set("Hi");
+        System.out.println(myStringProperty.get());
+        myStringProperty.set("Hello");
+        System.out.println(myStringProperty.get());
+        Property<Integer> myIProperty = new Property<Integer>();
+        myIProperty.set(5);
+        System.out.println(myIProperty.get());
+        myIProperty.set(6);
+        System.out.println(myIProperty.get());
+        Property<Boolean> myBProperty = new Property<Boolean>();
+        myBProperty.set(true);
+        System.out.println(myBProperty.get());
+        myBProperty.set(false);
+        System.out.println(myBProperty.get());
+
     }
 }
