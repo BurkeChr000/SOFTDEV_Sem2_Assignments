a87ad4d22a24710815a211567db5bccd3d8847d2
BurkeChr000 <burkechr000@warren.k12.in.us>
2024-02-06 10:47:29 -0500

    comt

--- 04_Functional Programming_Streams/01_Review of Functional Programming/Assignment/src/App.java
+++ 04_Functional Programming_Streams/01_Review of Functional Programming/Assignment/src/App.java
@@ -1,5 +1,9 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        System.out.println(LawOfCosinesA.apply(1.0, 1.0, 1.0));
     }
-}
+
+    public static TriFunction<Double, Double, Double, Double> LawOfCosinesA = (a, b, c) -> {
+        return Math.toDegrees(Math.acos((Math.pow(a, 2) + Math.pow(b, 2) - Math.pow(c, 2)) / (2 * a * b)));
+    };
+} 
