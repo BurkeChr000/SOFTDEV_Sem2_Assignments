bee9f787126bfd376829f25fa83af934306a67cc
BurkeChr000 <burkechr000@warren.k12.in.us>
2024-02-09 10:46:21 -0500

    comt

--- 04_Functional Programming_Streams/03_Optionals/Assignment/src/App.java
+++ 04_Functional Programming_Streams/03_Optionals/Assignment/src/App.java
@@ -1,7 +1,13 @@
+import java.util.function.*;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        
-    }
+        int testNum = 1;
 
-    Function 
+        Function<Integer, Integer> subtract5 = (num) -> num - 5;
+        Function<Integer, Integer> times5 = (num) -> num * 5;
+        Function<Integer, Integer> subtract5times5 = subtract5.andThen(times5);
+
+        System.out.println(subtract5times5.apply(testNum));
+    }
 }
